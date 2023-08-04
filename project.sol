d10fe3790a64ca806a2ef2c3a7357250e634a500
Infini9te <95559909+Infini9te@users.noreply.github.com>
2023-08-04 11:57:25 +0530

    Update project.sol

--- project.sol
+++ project.sol
@@ -1,52 +1,97 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "hardhat/console.sol";
+contract DegenToken_FREE {
+    string public name = "UniqueDegen";
+    string public symbol = "UDGN";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
 
-contract UniqueToken is ERC20, Ownable, ERC20Burnable {
+    address public owner;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor() ERC20("Degen", "DGN") {}
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Mint(address indexed to, uint256 amount);
+    event Burn(address indexed from, uint256 amount);
+    event Redeem(address indexed from, string item, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    constructor(uint256 initialSupply) {
+        owner = msg.sender;
+        totalSupply = initialSupply * 10 ** uint256(decimals);
+        balanceOf[owner] = totalSupply;
+    }
 
     function mintTokens(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+        require(to != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than zero");
+        require(totalSupply + amount >= totalSupply, "Total supply overflow");
+
+        balanceOf[to] += amount;
+        totalSupply += amount;
+        emit Mint(to, amount);
+        emit Transfer(address(0), to, amount);
     }
 
-    function transferTokens(address recipient, uint256 amount) external {
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        approve(msg.sender, amount);
-        transferFrom(msg.sender, recipient, amount);
+    function transferTokens(address to, uint256 amount) public {
+        require(to != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
     }
 
-    function getBalance() external view returns (uint256) {
-        return balanceOf(msg.sender);
+    function approveTokens(address spender, uint256 amount) public {
+        require(spender != address(0), "Invalid address");
+
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
     }
 
-    function burnTokens(uint256 amount) external {
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-        _burn(msg.sender, amount);
-    }
-    function gameStore() public pure returns (string memory) {
-        return "1. Special Item 1 - 200 Tokens\n2. Special Item 2 - 100 Tokens\n3. Special Item 3 - 75 Tokens";
+    function transferFromTokens(address from, address to, uint256 amount) public {
+        require(from != address(0), "Invalid address");
+        require(to != address(0), "Invalid address");
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        require(allowance[from][msg.sender] >= amount, "Allowance exceeded");
+
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
+        emit Transfer(from, to, amount);
     }
 
-    function redeemTokens(uint256 choice) external payable {
-        require(choice <= 3, "Invalid selection");
+    function burnTokens(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
 
-        if (choice == 1) {
-            require(balanceOf(msg.sender) >= 200, "Insufficient balance");
-            approve(msg.sender, 200);
-            transferFrom(msg.sender, owner(), 200);
-        } else if (choice == 2) {
-            require(balanceOf(msg.sender) >= 100, "Insufficient balance");
-            approve(msg.sender, 100);
-            transferFrom(msg.sender, owner(), 100);
-        } else {
-            require(balanceOf(msg.sender) >= 75, "Insufficient balance");
-            approve(msg.sender, 75);
-            transferFrom(msg.sender, owner(), 75);
-        }
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Burn(msg.sender, amount);
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+    mapping(string => uint256) public itemPrices;
+
+    function addItem(string memory itemName, uint256 price) public onlyOwner {
+        require(price > 0, "Price must be greater than zero");
+        itemPrices[itemName] = price;
+    }
+
+    function redeemTokens(string memory itemName) public {
+        uint256 itemPrice = itemPrices[itemName];
+        require(itemPrice > 0, "Invalid item");
+        require(balanceOf[msg.sender] >= itemPrice, "Insufficient balance");
+
+        balanceOf[msg.sender] -= itemPrice;
+        emit Redeem(msg.sender, itemName, itemPrice);
     }
 }
